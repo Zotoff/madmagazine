356541edbdcbfb2d8e3255261a94419acde40122
Zotoff <paulzotoff@gmail.com>
2020-11-23 10:52:10 +0300

    Главная - вдогонку, не все файлы проиндексировались

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -38,6 +38,9 @@ const mobileNav = document.querySelector(`#headerMobileNav`);
 const mobileMenu = document.querySelector(`#headerMobileMenu`);
 const mobileClose = document.querySelector(`#headerMobileClose`);
 const mobileSubscribe = mobileMenu.querySelector(`#headerMobileSubscribe`);
+const footerSubscribeForm = document.querySelector(`#footerSubscribe`);
+const headerSubscribeButton = document.querySelector(`#headerSubscribeButton`);
+
 mobileNav.addEventListener(`click`, (evt) => {
   evt.preventDefault();
   mobileMenu.classList.add(`show`);
@@ -47,8 +50,16 @@ mobileClose.addEventListener(`click`, (evt) => {
   mobileMenu.classList.remove(`show`);
 });
 
+headerSubscribeButton.addEventListener(`click`, (evt)=> {
+  evt.preventDefault();
+  footerSubscribeForm.scrollIntoView({block: "center", behavior: "smooth"});
+});
+
+
 if (mobileSubscribe) {
-  mobileSubscribe.addEventListener(`click`, () => {
+  mobileSubscribe.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    footerSubscribeForm.scrollIntoView({block: "center", behavior: "smooth"});
     mobileMenu.classList.remove(`show`);
   });
 }
